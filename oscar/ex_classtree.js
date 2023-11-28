00a5a3a1fdee81abdbfa048f84531e2f8415474a
situx <situx@users.noreply.github.com>
2023-11-28 14:52:43 +0000

    Deploying to gh-pages from @ situx/sampleRTImetadata@2651b10643e8cfc2b440208487be966be1cb7f52 🚀

--- oscar/ex_classtree.js
+++ oscar/ex_classtree.js
@@ -14130,7 +14130,7 @@ var tree={
           "from": {
             "http://objects.mainzed.org/ont#usesCamera": {
               "instancecount": 0,
-              "http://objects.mainzed.org/ont#Measurement": 10
+              "http://objects.mainzed.org/ont#Measurement": 484
             }
           }
         },
@@ -14353,7 +14353,7 @@ var tree={
             },
             "http://objects.mainzed.org/ont#usesLightSourceGroup": {
               "instancecount": 0,
-              "http://objects.mainzed.org/ont#Measurement": 14
+              "http://objects.mainzed.org/ont#Measurement": 20
             }
           }
         },
