3124a00fb60029a153b26e4364a457ead4cff898
situx <situx@users.noreply.github.com>
2023-11-29 17:38:49 +0000

    Deploying to gh-pages from @ situx/sampleRTImetadata@45c6027befd5e717a2d87d266344229d870cdfa0 🚀

--- oscar/minivowl_result.js
+++ oscar/minivowl_result.js
@@ -73,39 +73,18 @@ var minivowlresult={
  ],
  "links": [
   {
-   "source": 1,
-   "target": 10,
-   "valueTo": "pixelType",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#pixelType"
-  },
-  {
-   "source": 1,
-   "target": 11,
-   "valueTo": "usesLightSourceGroup",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#usesLightSourceGroup"
-  },
-  {
-   "source": 7,
-   "target": 8,
-   "valueTo": "hasValue",
-   "propertyTo": "class",
-   "uriTo": "http://www.ontology-of-units-of-measure.org/resource/om-2/hasValue"
-  },
-  {
-   "source": 1,
-   "target": 12,
-   "valueTo": "capturingdevice",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#capturingdevice"
-  },
-  {
    "source": 6,
    "target": 7,
    "valueTo": "exposureTime",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2003/12/exif/exposureTime"
+  },
+  {
+   "source": 1,
+   "target": 6,
+   "valueTo": "setup",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#setup"
   },
   {
    "source": 1,
@@ -116,10 +95,31 @@ var minivowlresult={
   },
   {
    "source": 1,
-   "target": 6,
-   "valueTo": "setup",
+   "target": 11,
+   "valueTo": "usesLightSourceGroup",
    "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#setup"
+   "uriTo": "http://objects.mainzed.org/ont#usesLightSourceGroup"
+  },
+  {
+   "source": 1,
+   "target": 10,
+   "valueTo": "pixelType",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#pixelType"
+  },
+  {
+   "source": 1,
+   "target": 12,
+   "valueTo": "capturingdevice",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#capturingdevice"
+  },
+  {
+   "source": 7,
+   "target": 8,
+   "valueTo": "hasValue",
+   "propertyTo": "class",
+   "uriTo": "http://www.ontology-of-units-of-measure.org/resource/om-2/hasValue"
   },
   {
    "source": 12,
@@ -129,18 +129,18 @@ var minivowlresult={
    "uriTo": "http://objects.mainzed.org/ont#hasLightSourceGroup"
   },
   {
-   "source": 12,
-   "target": 4,
-   "valueTo": "hasComponent",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#hasComponent"
-  },
-  {
    "source": 0,
    "target": 5,
    "valueTo": "measurementSeries",
    "propertyTo": "class",
    "uriTo": "http://objects.mainzed.org/ont#measurementSeries"
+  },
+  {
+   "source": 12,
+   "target": 4,
+   "valueTo": "hasComponent",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#hasComponent"
   }
  ]
 }
