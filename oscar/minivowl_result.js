730a4f051b38562f9cb7414b31c9f929ae772f9c
situx <situx@users.noreply.github.com>
2023-11-30 13:16:40 +0000

    Deploying to gh-pages from @ situx/sampleRTImetadata@37b7384f283402beaac4227e88c7352706ed1bea 🚀

--- oscar/minivowl_result.js
+++ oscar/minivowl_result.js
@@ -74,38 +74,10 @@ var minivowlresult={
  "links": [
   {
    "source": 1,
-   "target": 12,
-   "valueTo": "capturingdevice",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#capturingdevice"
-  },
-  {
-   "source": 6,
-   "target": 7,
-   "valueTo": "exposureTime",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2003/12/exif/exposureTime"
-  },
-  {
-   "source": 1,
-   "target": 11,
-   "valueTo": "usesLightSourceGroup",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#usesLightSourceGroup"
-  },
-  {
-   "source": 1,
    "target": 10,
    "valueTo": "pixelType",
    "propertyTo": "class",
    "uriTo": "http://objects.mainzed.org/ont#pixelType"
-  },
-  {
-   "source": 1,
-   "target": 9,
-   "valueTo": "usesCamera",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#usesCamera"
   },
   {
    "source": 7,
@@ -116,10 +88,38 @@ var minivowlresult={
   },
   {
    "source": 1,
+   "target": 11,
+   "valueTo": "usesLightSourceGroup",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#usesLightSourceGroup"
+  },
+  {
+   "source": 6,
+   "target": 7,
+   "valueTo": "exposureTime",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2003/12/exif/exposureTime"
+  },
+  {
+   "source": 1,
+   "target": 9,
+   "valueTo": "usesCamera",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#usesCamera"
+  },
+  {
+   "source": 1,
    "target": 6,
    "valueTo": "setup",
    "propertyTo": "class",
    "uriTo": "http://objects.mainzed.org/ont#setup"
+  },
+  {
+   "source": 1,
+   "target": 12,
+   "valueTo": "capturingdevice",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#capturingdevice"
   },
   {
    "source": 12,
@@ -129,18 +129,18 @@ var minivowlresult={
    "uriTo": "http://objects.mainzed.org/ont#hasLightSourceGroup"
   },
   {
-   "source": 12,
-   "target": 4,
-   "valueTo": "hasComponent",
-   "propertyTo": "class",
-   "uriTo": "http://objects.mainzed.org/ont#hasComponent"
-  },
-  {
    "source": 0,
    "target": 5,
    "valueTo": "measurementSeries",
    "propertyTo": "class",
    "uriTo": "http://objects.mainzed.org/ont#measurementSeries"
+  },
+  {
+   "source": 12,
+   "target": 4,
+   "valueTo": "hasComponent",
+   "propertyTo": "class",
+   "uriTo": "http://objects.mainzed.org/ont#hasComponent"
   }
  ]
 }
